16ea1ede22debc0c75365c167a0f529538e326d8
Emma Grey <emgrey02@gmail.com>
2021-12-13 16:11:30 -0500

    Complete cowbarn

---
+++ src/commands/cowbarn.ts
@@ -0,0 +1,69 @@
+import * as cowsay from 'cowsay';
+import { IOptions } from 'cowsay';
+import getRandomInt from '../utils/random';
+import quotes from '../utils/quotes.json';
+import { Message, MessageEmbed } from 'discord.js';
+import { channel } from 'diagnostics_channel';
+
+export default {
+  callback: (message: Message, ...args: string[]) => {
+    //generate random num
+    let randomNum = getRandomInt(0, quotes.length);
+
+    //use template literals to insert random quote
+    let opts: IOptions = {
+      text: 'hi',
+      f: args[0],
+    };
+
+    let output: string = cowsay.say(opts).slice(20);
+
+    let fixedOutput = '';
+
+    if (output.includes(`\`\`\``)) {
+      fixedOutput = output.replace(`\`\`\``, `'''`);
+    }
+
+    let finalOutput = `\`\`\`${fixedOutput ? fixedOutput : output}\`\`\``;
+
+    const cowbarn = new MessageEmbed()
+      .setColor('#9693EB')
+      .setTitle(`Hello there, human`)
+      .setDescription(`${finalOutput}`)
+      .addField(
+        `As ${quotes[randomNum].author} Once Said...`,
+        `${quotes[randomNum].quote}`,
+        true
+      )
+      .addField('\u200B', '\u200B')
+      .addField('Animal:', `${args[0]}`, true)
+      .addField('You win:', 'a cool dog pic', true)
+      .setImage(
+        'https://res.cloudinary.com/greypse/image/upload/w_200,h_200,c_fill,e_hue:-85/IMG_8813_dmjsgp.jpg'
+      )
+      .setTimestamp()
+      .setFooter(
+        '\u2800'.repeat(40),
+        'https://res.cloudinary.com/greypse/image/upload/v1639428779/discord-bot_gxtcrj.png'
+      );
+
+    message.channel
+      .send({ embeds: [cowbarn] })
+      .then(() => {
+        message
+          .react('🍄')
+          .then(() => console.log('cowsay reaction sent.'))
+          .catch(console.error);
+        console.log('success');
+      })
+      .catch((err) => {
+        if (err.code === 50035) {
+          message
+            .reply(`I can't post that...`)
+            .then(() => console.log('error message sent'))
+            .catch(console.error);
+        }
+        console.error(err);
+      });
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import DiscordJS, { Intents } from 'discord.js';
 import 'dotenv/config';
-import dotenv from 'dotenv';
-import cowsay from './commands/cowsay';
 
 //create client
 const client = new DiscordJS.Client({
@@ -17,33 +15,3 @@ client.on('ready', () => {
 });
 
 client.login(process.env.TOKEN);
-
-// client.on('messageCreate', (message) => {
-//
-
-//   //separate user commands into an array of strings
-//   let args = message.content.toLowerCase().slice(3).trim().split(' ');
-
-//   if (args.includes('ping')) {
-//     message
-//       .reply('pong')
-//       .then(() => console.log('pong reply sent.'))
-//       .catch(console.error);
-//     message
-//       .react('🐌')
-//       .then(() => console.log('ping reaction sent.'))
-//       .catch(console.error);
-//     message
-//       .reply('I will never miss!')
-//       .then(() => console.log('custom pong reply sent.'))
-//       .catch(console.error);
-//   }
-
-// if (args.includes('cowsay')) {
-//   //filter out args
-//   let followingArgs = args.filter((arg) => arg != 'cowsay' && arg != 'ping');
-
-//   message
-//     .react('🍄')
-//     .then(() => console.log('cowsay reaction sent.'))
-//     .catch(console.error);
